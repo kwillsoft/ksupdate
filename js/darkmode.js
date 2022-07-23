1d962e41ab6767123a935566c3e401c3a360a0f4
kwillsoft <kwillsoftware@gmail.com>
2022-07-22 21:49:28 -0700

    Add files via upload

---
+++ js/darkmode.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $(".btn").click(function() {
+      $(".content1").toggleClass("active");
+    });
+  });
+  $(document).ready(function() {
+    $(".btn").click(function() {
+      $(".content2").toggleClass("active");
+    });
+  });
+
+  
